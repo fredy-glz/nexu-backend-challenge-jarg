e95b9be14699291c91c8ccf3bcdf71734f0d1953
Alfredo Romero González <fredyglz.fr@gmail.com>
2024-02-09 23:35:43 -0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server on port ${process.env.PORT}`)
+);
